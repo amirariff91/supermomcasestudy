f3ca1728eaeb6cc706d3c83c566d4fe846cf3d78
amirariff <ariffseh@gmail.com>
2025-03-10 17:22:12 +0800

    Refine Next.js image configuration with precise remote patterns

--- next.config.ts
+++ next.config.ts
@@ -6,19 +6,22 @@ const nextConfig: NextConfig = {
   compress: true,
   output: 'standalone',
   images: {
-    domains: ['localhost', 'd844w4gc8w0ks4kks4o0csk0.5.223.50.174.sslip.io'],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: '**',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'http',
+        hostname: 'd844w4gc8w0ks4kks4o0csk0.5.223.50.174.sslip.io',
       },
       {
         protocol: 'https',
-        hostname: '**',
-      },
+        hostname: 'd844w4gc8w0ks4kks4o0csk0.5.223.50.174.sslip.io',
+      }
     ],
   },
   typescript: {
